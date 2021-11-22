866bca3414082aba812129f38bf5c27635e7ac8b
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-11-22 15:34:47 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data(){
+    return{
+      navbar: [
+        {text: "Home", link: "/home"},
+        {text: "Apple", link: "/apple"},
+        {text: "Microsoft", link: "/microsoft"},
+        {text: "Android", link: "/android"},
+        {text: "Forums", link: "/forums"},
+        {text: "Contact us", link: "/contact-us"},
+      ],
+    }
+  },
+}
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data(){
+    return{
+      articleTop: [
+        { 
+          img: require(""),
+          title: "Morbi vitae dui euismod vulputate sollicitudin",
+          subTitle: "October 11th, 2015 | 2 Comments",
+          text: "loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem"
+          },
+        { 
+          img: require(""),
+          title: "Vivamus pellenteque, felis qui varius",
+          subTitle: "October 11th, 2015 | Comments OTT",
+          text: "loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem"
+          },
+        { 
+          img: require(""),
+          title: "Donec ornare pretium eget scelisque justo",
+          subTitle: "October 11th, 2015 | Comments OTT",
+          text: "loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem"
+          },
+      ],
+    }
+  },
+}
+</script>
